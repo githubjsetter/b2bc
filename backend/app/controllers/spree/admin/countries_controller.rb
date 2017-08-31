5201f0676e767e1656f5351f09a801ade4ddf371
jsetter <327115417@qq.com>
2017-08-31 15:20:39 +0800

    FIRST

---
+++ backend/app/controllers/spree/admin/countries_controller.rb
@@ -0,0 +1,12 @@
+module Spree
+  module Admin
+    class CountriesController < ResourceController
+
+      private
+        def collection
+          super.order(:name)
+        end
+
+    end
+  end
+end
