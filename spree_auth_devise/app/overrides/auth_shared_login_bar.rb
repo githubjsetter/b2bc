c2191aec97c511508b68afa17479a377c1f0b440
jsetter <327115417@qq.com>
2017-09-07 16:12:33 +0800

    Signed-off-by: jsetter <327115417@qq.com>

--- spree_auth_devise/app/overrides/auth_shared_login_bar.rb
+++ spree_auth_devise/app/overrides/auth_shared_login_bar.rb
@@ -3,4 +3,4 @@ Deface::Override.new(virtual_path: "spree/shared/_nav_bar",
                      insert_before: "li#search-bar",
                      partial: "spree/shared/login_bar",
                      disabled: false,
-                     original: 'c00d67dfc0496d21a351b48c41a728412cb5ed2c')
+                     original: '71d12ae155143c6ea8090942955299485f534153')
